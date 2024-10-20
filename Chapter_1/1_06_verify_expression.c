affb450775a6f1a0ca7e6edffbbbeeeae55df98c
SikuAG <ajimcon@protonmail.com>
2024-10-20 12:26:31 +0200

    Added two new exercices

---
+++ Chapter_1/1_06_verify_expression.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Value of expression EOF: %d\n", getchar() != EOF);
+	return (0);
+}
---
+++ Chapter_1/1_07_print_eof.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("EOF Value: %d", EOF);
+	return (0);
+}
